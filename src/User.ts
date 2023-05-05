1a205a0f3c5fab53e3b244b4141467d29c57dca8
KrysDev <webdevkrys@gmail.com>
2023-05-05 21:15:27 +0100

    x

--- src/User.ts
+++ src/User.ts
@@ -1,11 +1,24 @@
-import { faker } from "@faker-js/faker";
+import { faker } from '@faker-js/faker';
 
-class User {
+// named export (so to import in index.ts the {} are needed) 
+export class User {
+  // we have only type annotations below, the properties have not been initialized
   name: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    // initialize 'name'
+    this.name = faker.name.firstName();
+
+    // initialize 'location' as an object with lat and lng
+    // we do parseFloat to convert the faker generated lat and lng strings to numbers
+    this.location = {
+      // lat: faker.address.longitude() --> string
+      lat: parseFloat(faker.address.latitude()), // number
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('start');
+import { User } from './User';
+
+const user = new User();
+
+console.log(user);
